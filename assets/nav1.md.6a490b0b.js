ba052047c276c1e4e0d61a3362ca322800fea775
txtxtxama <txtxtxama@users.noreply.github.com>
2022-08-17 16:58:15 +0000

    deploy: 49f8fd39bc3247d660fc58ce42a6a7d914e08f73

---
+++ assets/nav1.md.6a490b0b.js
@@ -0,0 +1,1 @@
+import{_ as e,c as a,o as t,a as n}from"./app.3ad82576.js";const v=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"nav1.md"}'),o={name:"nav1.md"},s=n("p",null,"nav1",-1),r=[s];function c(_,d,p,i,l,m){return t(),a("div",null,r)}var u=e(o,[["render",c]]);export{v as __pageData,u as default};
